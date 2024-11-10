e97dd36040a10f1aa9c4d1ab87d011f7db6ac0c7
gustavogk <gust.krv@gmail.com>
2024-11-10 19:45:50 -0300

    feat: config next auth and connection with google calendar

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -48,6 +48,8 @@ export default function Register() {
         username: data.username,
         name: data.name,
       });
+
+      await router.push("/register/connect-calendar");
     } catch (error) {
       if (error instanceof AxiosError && error.response?.data?.error) {
         alert("Usuário já existe");
