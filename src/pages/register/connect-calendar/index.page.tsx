72733b3cc1b4a034add9e82793e3b2e4501601db
gustavogk <gust.krv@gmail.com>
2024-11-13 23:08:17 -0300

    feat: implementing update profile process

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -16,6 +16,10 @@ export default function ConnectCalendar() {
     await signIn("google");
   }
 
+  async function handleNavigateToNextStep() {
+    await router.push("/register/time-intervals");
+  }
+
   return (
     <Container>
       <Header>
@@ -53,7 +57,11 @@ export default function ConnectCalendar() {
             permissões de acesso ao Google Calendar.
           </AuthError>
         )}
-        <Button type="submit" disabled={!isSigningIn}>
+        <Button
+          onClick={handleNavigateToNextStep}
+          type="submit"
+          disabled={!isSigningIn}
+        >
           Próximo passo
           <ArrowRight />
         </Button>
