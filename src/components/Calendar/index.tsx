fe57d43f5a30faee3919829f14be506973ad05f7
gustavogk <gust.krv@gmail.com>
2024-11-15 19:42:21 -0300

    feat: implementing schedule page style

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,68 @@
+import {
+  CalendarContainer,
+  CalendarHeader,
+  CalendarTitle,
+  CalendarActions,
+  CalendarBody,
+  CalendarDay,
+} from "./styles";
+
+import { CaretLeft, CaretRight } from "phosphor-react";
+import { getWeekDays } from "@/utils/get-week-days";
+
+export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true });
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Dezembro <span>2022</span>
+        </CalendarTitle>
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  );
+}
