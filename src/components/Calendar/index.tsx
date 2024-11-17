c9b3faaf004a3198c65ad9754b09b44269d9311d
gustavogk <gust.krv@gmail.com>
2024-11-17 20:23:45 -0300

    feat: add dayjs + calendar improvement

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -9,21 +9,40 @@ import {
 
 import { CaretLeft, CaretRight } from "phosphor-react";
 import { getWeekDays } from "@/utils/get-week-days";
+import { useState } from "react";
+import dayjs from "dayjs";
 
 export function Calendar() {
+  const [currentDate, setCurentDate] = useState(() => {
+    return dayjs().set("date", 1);
+  });
+
+  function handlePreviusMonth() {
+    const handlePreviusMonth = currentDate.subtract(1, "month");
+    setCurentDate(handlePreviusMonth);
+  }
+
+  function handleNextMounth() {
+    const handlePreviusMonth = currentDate.add(1, "month");
+    setCurentDate(handlePreviusMonth);
+  }
+
   const shortWeekDays = getWeekDays({ short: true });
+
+  const currentMonth = currentDate.format("MMMM");
+  const currentYear = currentDate.format("YYYY");
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Dezembro <span>2022</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviusMonth} title="Previus Mounth">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMounth} title="Next Mounth">
             <CaretRight />
           </button>
         </CalendarActions>
