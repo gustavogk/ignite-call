5427ad05406e71be517abbe1c84399564b74ed6f
gustavogk <gust.krv@gmail.com>
2024-12-02 21:42:10 -0300

    feat: config google API

--- src/pages/register/update-profile/index.page.tsx
+++ src/pages/register/update-profile/index.page.tsx
@@ -42,7 +42,7 @@ export default function UpdateProfile() {
       bio: data.bio,
     });
 
-    await router.push("/schedule/${session.data?.user.username}");
+    await router.push(`/schedule/${session.data?.user.username}`);
   }
 
   return (
